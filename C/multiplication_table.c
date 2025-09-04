060969e968503baa8594c2c0a99dc7ca39345c59
Denny Jose T <denny.jose@lyptus-tech.com>
2025-09-04 23:57:45 +0530

    initial commit

---
+++ C/multiplication_table.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void multiplication_table(int num)
+{
+
+    int i = 0;
+    while(i<=10){
+
+        printf ("%d ", num * i );
+        i = i+1;
+    }
+
+}
+
+int main()
+{
+    int n;
+}
